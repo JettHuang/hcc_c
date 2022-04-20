2cb4865583edf1d82720942fa3d6b04d02ceaf52
JettHuang <123258314@qq.com>
2022-04-20 16:47:55 +0800

    add dist\include files

---
+++ dist/ReadMe.txt
@@ -0,0 +1,1 @@
+include is copy from tcc-0.9.25
--- dist/tests/test0.h
+++
@@ -1,9 +0,0 @@
-/*
- * This is for testing cpp
- */
- 
-#define JOIN(x, y)  x##y
-#define MIN(x, y) (x < y ? x : y)
-
-#define XYZ  x
-#define ABC  XYZ
